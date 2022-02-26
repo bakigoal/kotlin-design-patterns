d0105dd1c1cece8b2a13263f68f2b2f842f530e7
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-02-26 22:30:14 +0500

    delegate dog creation to dog factory

--- src/test/kotlin/creational/factoryMethod/FactoryMethodTest.kt
+++ src/test/kotlin/creational/factoryMethod/FactoryMethodTest.kt
@@ -8,8 +8,16 @@ internal class FactoryMethodTest {
 
     @Test
     fun `read from config then creates animals`() {
-        val animalTypes = listOf("dog" to "buldog", "dog" to "doberman", "cat" to "")
-        val expected = listOf(Buldog::class.java, Doberman::class.java, Cat::class.java)
+        val animalTypes = listOf(
+            "dog" to "buldog",
+            "dog" to "doberman",
+            "cat" to ""
+        )
+        val expected = listOf(
+            Buldog::class.java,
+            Doberman::class.java,
+            Cat::class.java
+        )
         val actual = animalTypes.stream()
             .map { (type, breed) -> animalFactory(type, breed) }
             .map { it::class.java }
